35ff2db420530032e712f794ceb3d194671b68b6
Robin TOURNÉ <robin.tourne@ynov.com>
2022-11-01 10:21:50 +0100

    update: display lab component when category clicked

--- src/routes/labs/+page.svelte
+++ src/routes/labs/+page.svelte
@@ -5,15 +5,30 @@
 	import { onMount } from 'svelte';
 	import { flip } from 'svelte/animate';
 	import { fade, fly } from 'svelte/transition';
+	import type Component from 'svelte/types/compiler/compile/Component';
 	import { getCurrentBreakPoint } from '../../utils/tailwind-helper';
 	import CategoryItem from './CategoryItem.svelte';
+	import CssChallenges from './CssChallenges.svelte';
+	import DockerLabs from './DockerLabs.svelte';
+	import WebDev from './WebDev.svelte';
 
 	let blockTranslate = { value: 'translateY(0px)', locked: false };
 	let destination = '';
 	let visible = true;
 	let selectedCategory = '';
+	let selectedComponent;
+	let itemsBeenRemoved = false;
 
-	const categories = ['Web Development', 'CSS Challenges', 'Docker Labs'];
+	const categories = [{
+		name: 'Web Development',
+		component: WebDev,
+	}, {
+		name: 'CSS Challenges',
+		component: CssChallenges,
+	}, {
+		name: 'Docker Labs',
+		component: DockerLabs,
+	}];
 
 	onMount(() => {
 		document.body.style.overflow = 'unset';
@@ -29,35 +44,52 @@
 
 	const beforeNavigating = (event) => {
 		destination = event.detail;
+		if (selectedCategory || selectedComponent) {
+			selectedComponent = null;
+			selectedCategory = '';
+			itemsBeenRemoved = false;
+			return;
+		}
 		visible = false;
-		goto(destination)
+		goto(destination);
 	};
 
-	const onCategorySelected = (category: 'Web Development' | 'CSS Challenges' | 'Docker labs') => {
+	const onCategorySelected = (category: { name: string, component: Component }) => {
 		blockTranslate.locked = true;
-		selectedCategory = category;
+		selectedCategory = category.name;
+		selectedComponent = category.component;
 	};
 </script>
 
 <AnimationFragment visible={visible}>
-  <div transition:fade >
-    <Back links={[{name: "Home", href: "/"}]} on:navigate={beforeNavigating}/>
+  <div transition:fade>
+    <Back links={[{name: "Back", href: "/"}]} on:navigate={beforeNavigating}/>
     <div style={`transform: ${blockTranslate.value}`} class="
     flex flex-col gap-4 w-screen lg:mx-auto select-none mt-4 lg:mt-6
     [&_h2>*]:font-poppins-bold [&_h2>*]:text-5xl
   ">
-      {#each categories.filter((it) => !selectedCategory || it === selectedCategory) as category, index (category)}
+      {#each categories.filter((it) => !selectedCategory || it.name === selectedCategory) as {
+        name,
+        component
+      }, index (name)}
         <div
-          on:keydown={() => onCategorySelected(category)}
-          on:click={() => onCategorySelected(category)}
+          on:keydown={() => onCategorySelected({name, component})}
+          on:click={() => onCategorySelected({name, component})}
+          in:fly={{x: -500, duration: 300, delay: 400}}
           out:fly={{x: -500, duration: 300, delay: 0}}
+          on:outroend={() => (itemsBeenRemoved = true)}
           animate:flip={{delay: 100, duration: 400}}
         >
           {#key selectedCategory}
-            <CategoryItem name={category} expanded={selectedCategory && selectedCategory === category}/>
+            <CategoryItem name={name} expanded={selectedCategory && selectedCategory === name}/>
           {/key}
         </div>
       {/each}
+      {#if selectedCategory && selectedComponent && itemsBeenRemoved}
+        <div in:fly={{x: 500, duration:300}}>
+          <svelte:component this={selectedComponent}></svelte:component>
+        </div>
+      {/if}
     </div>
   </div>
 </AnimationFragment>
