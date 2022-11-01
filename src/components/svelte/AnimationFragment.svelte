376a6b5939ffad8042b44bb133877042a9b707f5
Robin TOURNÉ <robin.tourne@ynov.com>
2022-11-01 14:57:47 +0100

    change: not focusing on SSR anymore / add: Favicon

--- src/components/svelte/AnimationFragment.svelte
+++ src/components/svelte/AnimationFragment.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
   import { onMount } from 'svelte';
 
+	const OPTIMISE_SSR = false;
 	let opacity = 0;
 	export let className = '';
-	export let visible = true;
+	export let visible = OPTIMISE_SSR;
 
 	onMount(() => {
     opacity = 1;
-		visible = false;
-		setTimeout(() => (visible = true), 10);
+		visible = !OPTIMISE_SSR;
+		if (OPTIMISE_SSR) setTimeout(() => (visible = true), 350);
   });
 </script>
 
-<div style="opacity: {opacity}" class={className}>
+<div class={className}>
   {#if visible}
     <slot />
   {/if}
--- src/routes/testing/+page.svelte
+++ src/routes/testing/+page.svelte
@@ -1,28 +1,5 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
-	import AnimationFragment from '@components/svelte/AnimationFragment.svelte';
-	import Back from '@components/navigation/Back.svelte';
-	import { goto } from "$app/navigation";
-
-	let visible = true;
-  let destination = '';
-
-	const beforeNavigating = (event) => {
-		destination = event.detail;
-		visible = false;
-  }
+	import BgDecoration from '@components/background/BgDecoration.svelte';
 </script>
 
-<div class="h-screen flex flex-col justify-center items-center">
-  <Back links={[{name: 'Home', href: '/'}]} on:navigate={beforeNavigating}/>
-  <AnimationFragment visible={visible}>
-    <h1 class="text-5xl font-poppins-bold"
-        in:fly={{y: 100, duration: 1000}}
-        out:fly={{y: 100, duration: 1000}}
-        on:outroend={() => goto(destination)}>
-    >
-      Testing things
-    </h1>
-  </AnimationFragment>
-  <button on:click={() => (visible = !visible)} class="border border-black border-4 p-4 rounded-xl m-4">Toggle Visible</button>
-</div>
+<BgDecoration height="30vh" width="30vh" posTop="50%" posLeft="50%" translateX="-50%" translateY="-50%" rotate="-45deg"/>
