78b3106e964d0a987346fb3e9f84d748cf9327ae
Robin TOURNÉ <robin.tourne@ynov.com>
2022-11-10 16:23:43 +0100

    add: css challenges

--- src/routes/labs/LabCard.svelte
+++ src/routes/labs/LabCard.svelte
@@ -7,7 +7,7 @@
 
 <div class="p-4 lg:mt-8 w-screen relative z-10 lg:w-3/4 mx-auto">
   <div
-    class="prose bg-white lg:p-4 rounded-xl lg:border lg:border-black lg:border-solid lg:border-2 max-w-full lg:drop-shadow-2xl">
+    class="prose bg-white lg:p-4 rounded-xl lg:border lg:border-black lg:border-solid lg:border-2 max-w-full lg:drop-shadow-2xl [&_img]:m-auto">
     <SvelteMarkdown source={content} />
   </div>
 </div>
